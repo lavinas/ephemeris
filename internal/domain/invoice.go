0dd29713916144178b5e6a3d8bc38af6ecbb6904
lavinas <lavinas@me.com>
2024-04-22 20:01:44 -0300

    Invoice domain

--- internal/domain/invoice.go
+++ internal/domain/invoice.go
@@ -33,7 +33,7 @@ type Invoice struct {
 	ID            string    `gorm:"type:varchar(25); primaryKey"`
 	Date          time.Time `gorm:"type:datetime; not null; index"`
 	ClientID      string    `gorm:"type:varchar(25); not null; index"`
-	Value         float64   `gorm:"type:numeric(20,2); not null"`
+	Value         float64   `gorm:"type:numeric(20,2); not null; index"`
 	Status        string    `gorm:"type:varchar(25); not null; index"`
 	SendStatus    string    `gorm:"type:varchar(25); not null; index"`
 	PaymentStatus string    `gorm:"type:varchar(25); not null; index"`
