26f92bd93d0d30ee2156069a4eabf3fdf84bdd1c
lavinas <lavinas@me.com>
2024-07-03 00:02:22 -0300

    WeigthEuclideanDistance on commands

--- pkg/commands.go
+++ pkg/commands.go
@@ -147,6 +147,35 @@ func (c *Commands) Sort(v interface{}, cmd string) {
 	c.sort(v, field, down)
 }
 
+// WeightedEuclidean returns the euclidean distance between x and y with weights w
+// and w is a slice of weights for each character in x and y
+// if w is nil, the weights are calculated as a decreasing sequence
+// starting in 1 and ending in 1/len(x)
+// returns the euclidean distance between x and y with weights w or an error
+func (c *Commands) WeightedEuclidean(x, y string, w []float64) (float64, error) {
+	if len(x) != len(y) {
+		return 0, fmt.Errorf("x, y must have the same length")
+	}
+	if w != nil && len(w) < len(x) {
+		return 0, fmt.Errorf("w must be at least the same length as x")
+	}
+	k := 1.00000000
+	dec := 1 / float64(len(x))
+	z := 0.00000000
+	for i := 0; i < len(x); i++ {
+		z1 := float64(x[i] - y[i])
+		z1 = z1 * z1
+		if w != nil {
+			z1 = z1 * w[i]
+		} else {
+			z1 = z1 * k
+			k -= dec
+		}
+		z += z1
+	}
+	return math.Sqrt(z), nil
+}
+
 // sortParams is a function that returns the field and the direction of a command
 func (c *Commands) sortParams(cmd string) (string, bool) {
 	field := strings.Split(cmd, " ")[0]
